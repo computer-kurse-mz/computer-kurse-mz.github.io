ac8e2bf9f289df3fe9e4b0f07a761141d02ef532
computer-kurse-mz <computer.kurse.mz@gmail.com>
2025-04-24 06:54:57 +0000

    dark terminal mode basic 	new file:   assets/icons/dark-mode.svg 	new file:   assets/icons/light-mode.svg 	new file:   assets/icons/theme.svg 	new file:   assets/videos/background18.mp4 	modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-// script.js
 function toggleMode() {
   document.body.classList.toggle('dark-mode');
-  const toggleButton = document.querySelector('.mode-toggle');
-  if (document.body.classList.contains('dark-mode')) {
-    toggleButton.textContent = '☀️';
-  } else {
-    toggleButton.textContent = '🌙';
-  }
 }
 
 // Playback-Rate des Videos
 document.addEventListener('DOMContentLoaded', function () {
   const video = document.querySelector('.bg-video');
   if (video) {
-    video.playbackRate = 0.3; // 0.5 = halb so schnell, 1 = normal
+    video.playbackRate = 0.6; // 0.5 = halb so schnell, 1 = normal
   }
 });
