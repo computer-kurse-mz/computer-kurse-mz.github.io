2bc3c9c9f7059ade8d1dd58d9d888473cf64aaa4
computer-kurse-mz <computer.kurse.mz@gmail.com>
2025-04-24 13:04:33 +0000

    zwischenstand 	modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -18,10 +18,15 @@ document.addEventListener('DOMContentLoaded', () => {
     video.playbackRate = 0.4;
   }
 
-  // Burger-Menü
   const burger = document.querySelector('.burger');
   const navLinks = document.querySelector('.nav-links');
   burger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
+  });
+});
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('open');
   });
 });
