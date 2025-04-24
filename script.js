f65a53b117fbaf76f19a9542e8914427c16d0e4d
computer-kurse-mz <computer.kurse.mz@gmail.com>
2025-04-24 05:51:20 +0000

    first styling amendments

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ function toggleMode() {
   }
 }
 
+// Playback-Rate des Videos
+document.addEventListener('DOMContentLoaded', function () {
+  const video = document.querySelector('.bg-video');
+  if (video) {
+    video.playbackRate = 0.3; // 0.5 = halb so schnell, 1 = normal
+  }
+});
